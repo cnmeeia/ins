51fee55bdb718328bb800ce198749b57d5ba07db
hello1983 <shayne125390@gmail.com>
2022-04-01 09:04:39 +0000

    03

--- image_02/README.md
+++
@@ -1,3 +0,0 @@
-#### instagram images
-
-[-CHzAirjn9U1]("https://m.app2022.ml/images/blog/-CVsP-myP7Ns.html")
--- image_02/dl.sh
+++
@@ -1,9 +0,0 @@
-cd ins_download
-
-instaloader --login=mick.io21 -- -CaIOoVCBuFn
-
-git add .
-
-git commit -m "instagram"
-
-git push
